ed3ac35fff3cc1b08f2f1afc154c3e046c3b4d19
Drew Cook <dc333@usf.edu>
2025-01-30 16:23:43 -0500

    Complete Task 2

---
+++ in_class_assignment_1.js
@@ -0,0 +1,22 @@
+// Task 1
+
+let x = 10;
+if (x > 5) {
+    console.log("Help")
+};
+
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses.");
+} else {
+    console.log("Manageable Expenses.");
+};
+
+
+//Task 2
+let score = 80
+if (score >= 60) {
+    console.log("Pass")
+} else {
+    console.log("Fail")
+}
